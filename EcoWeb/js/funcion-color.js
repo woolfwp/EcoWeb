b40afebbd9a8d042c48cb4fa6f1b0dbc82b662df
Sebastian Gomez Quinayas <sebastian.gomez@tj.local>
2022-03-18 11:20:54 -0500

    se comienza con body y funcion para cambiar de color

---
+++ EcoWeb/js/funcion-color.js
@@ -0,0 +1,10 @@
+(function(){
+    setInterval(function(){
+
+        var primeraLetra = document.getElementById('colorOne'),
+            segundaLetra = document.getElementById('colorSecond')
+
+    
+        
+    },1000);
+}())
