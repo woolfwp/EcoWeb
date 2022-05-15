6e10f02cb46a39463ad3af81a4a60075f8f38fc8
sebasPolar <sebasgomez1099@gmail.com>
2022-05-15 16:11:09 -0500

    arquitectura consulta de api rutas

--- EcoWeb/js/apis.js
+++ EcoWeb/js/apis.js
@@ -1,18 +1,23 @@
-
-const urlApiRuta = ''
+const urlApiRuta = 'http://localhost:8000/ruta/Pico%20Pnace';
 
 const cargarRutas = async() => {
     try {
+    
         const respuestaRutas = await fetch(urlApiRuta)
-
         const datosRuta = await respuestaRutas.json();
-        console.log(datosRuta.nombre);
+
+        let rutas = '';
+        datosRuta.nombre.forEach(ruta => {
+            rutas += '<h2>${datosRuta.nombre}</h2>';
+        });
+
+        document.getElementById('nombreRuta').innerHTML = rutas;
 
     } catch (error) {
         console.log(error);
     }
 
-  
+
 }
 
 cargarRutas();
