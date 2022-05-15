36bded0ba834e98ba0af66f6988975511a293931
andrees971 <andresfelipesm97@hotmail.com>
2022-05-15 15:41:29 -0500

    se oraganizo todas las rutas y se creo user caminante

--- EcoWeb/js/apis.js
+++ EcoWeb/js/apis.js
@@ -1,9 +1,11 @@
 
-const urlApiRuta = ''
+const urlApiRuta = 'http://localhost:8000/ruta/Pico%20Pnace'
 
 const cargarRutas = async() => {
     try {
+        console.log('paso 1', urlApiRuta)
         const respuestaRutas = await fetch(urlApiRuta)
+        console.log('paso 2')
 
         const datosRuta = await respuestaRutas.json();
         console.log(datosRuta.nombre);
